0a5328ead05b0f76c99598fb118d73fdf65dd089
Fernando Warno <ferwar@hotmail.com>
2023-12-16 05:23:03 -0300

    Modificaciones varias

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
+
+window.onload = function() {
+  const rootElement = document.getElementById('root');
+  if(rootElement) {
+    createRoot(rootElement).render(<App />);
+  } else {
+    console.error('Element with id "root" not found');
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
+const path = require('path');
+
 module.exports = {
+  mode: 'development',
+  entry: './frontend/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'frontend/static/frontend'),
+    filename: 'main.js',
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: '/node_modules/',
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
-}
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
