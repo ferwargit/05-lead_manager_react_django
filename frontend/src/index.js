e54f6b8c6ce16972c49078e115d9cf8e1533306d
Fernando Warno <ferwar@hotmail.com>
2023-12-16 14:51:46 -0300

    ajuste de archivo index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
-window.onload = function() {
-  const rootElement = document.getElementById('root');
-  if(rootElement) {
-    createRoot(rootElement).render(<App />);
-  } else {
-    console.error('Element with id "root" not found');
-  }
-}
+const rootElement = document.getElementById('root');
+createRoot(rootElement).render(<App />);
