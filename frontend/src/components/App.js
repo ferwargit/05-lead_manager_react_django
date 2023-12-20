986db9aa7098b3df2f5352db972e31937a836b79
Fernando Warno <ferwar@hotmail.com>
2023-12-20 18:16:26 -0300

    Register.js - Login.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,21 +2,31 @@ import React, { Component, Fragment } from "react";
 import Header from "./layout/Header.js";
 import Dashboard from "./leads/Dashboard.js";
 import Alerts from "./layout/Alerts.js";
+import Register from "./accounts/Register.js";
+import Login from "./accounts/Login.js";
+
 
 import { Provider } from "react-redux";
 import store from "../store.js";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <Alerts />
-          <div className="container">
-            <Dashboard />
-          </div>
-        </Fragment>
+        <Router>
+          <Fragment>
+            <Header />
+            <Alerts />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </div>
+          </Fragment>
+        </Router>
       </Provider>
     );
   }
